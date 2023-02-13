5de148fe26b7085a95081eee51e0c3773f331b50
Leonardo Colaleo <124148340+leon94H@users.noreply.github.com>
2023-02-13 18:10:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+// Scrivi una funzione di uguaglianza che prenda in input due argomenti e restituisca TRUE se i due argomenti sono IDENTICI,
+//   FALSE altrimenti.
+//   Esempi:func
+//     Input: n = 2, m = 3
+//     Output: FALSE
+//     Input: n = 2, m = '2'
+//     Output: FALSE
+//     Input: n = 2, m = 2
+//     Output: TRUE
+
+
+
+
+
+
+
+
+// let number = 10;
+// let number2 = 10; 
+
+// function numbers (input1,input2){
+//     if(input1 == input2){
+//         console.log(true);
+//     } else{
+//         console.log(false)}
+// }
+
+// numbers(number,number2);
+
+
+
+// let number = 10;
+// let number2 = 20; 
+
+// function numbers (input1,input2){
+//     if(input1 == input2){
+//         console.log(true);
+//     } else{
+//         console.log(false)}
+// }
+
+// numbers(number,number2);
+
+
+
+
+
+
+
+// Scrivi una funzione che dato un numero intero (massimo 9999) conti da quante cifre è formato.
+
+// Esempi:
+
+//     Input : 9
+//     Output :  1 cifra
+    
+//     Input : 99
+//     Output :  2 cifre 
+
+
+
+
+let number = "746";
+
+function countTheNumber(num){
+    return num.lenght
+}
+
+console.log(countTheNumber (number));
+
